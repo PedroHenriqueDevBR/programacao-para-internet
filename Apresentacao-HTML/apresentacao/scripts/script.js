7993d018145aa3887bacea805460e66953c387cf
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-03-23 15:41:31 -0300

    Apresentação da tag Head finalaizada

--- Apresentacao-HTML/apresentacao/scripts/script.js
+++ Apresentacao-HTML/apresentacao/scripts/script.js
@@ -1,8 +1,9 @@
-var fontsize = 12
+let fontsize = 12
 
 function listenerInit(){
     document.getElementById('increment').addEventListener('click', incrementLetter)
     document.getElementById('decrement').addEventListener('click', decrementLetter)
+    document.getElementById('tituloexemplo').addEventListener('click', writeMesage)
 }
 
 function incrementLetter() {
@@ -20,6 +21,20 @@ function decrementLetter(params) {
     for (element of elements) {
         element.style.fontSize = fontsize + 'pt'
         document.getElementById('config').style.fontSize = '12pt'
+    }
+}
+
+function writeMesage() {
+    let theElement = document.getElementById('tituloexemplo')
+    theElement.innerHTML = ''
+    let newMesage = 'Digo, mão no código'
+
+    for (let i = 0; i < newMesage.length; i++) {
+        setTimeout(() => {
+            theElement.innerHTML += newMesage[i]
+            // console.log(newMesage[i])
+            // console.log('ads')
+        }, 100 * i);
     }
 }
 
