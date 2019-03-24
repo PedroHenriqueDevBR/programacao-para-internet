649cf8619d5addccf537001dcd9c64480cf5556f
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-03-24 15:21:05 -0300

    Criação de botões de ação a página

--- Apresentacao-HTML/apresentacao/scripts/script.js
+++ Apresentacao-HTML/apresentacao/scripts/script.js
@@ -6,6 +6,25 @@ function listenerInit(){
     document.getElementById('tituloexemplo').addEventListener('click', writeMesage)
     document.getElementById('layoutmore').addEventListener('click', moreLayout)
     document.getElementById('layoutless').addEventListener('click', lessLayout)
+    document.getElementById('showMenu').addEventListener('click', showMenu)
+    document.getElementById('hideMenu').addEventListener('click', hideMenu)
+}
+
+function hideMenu() {
+    document.getElementById('cabecalho').style.display = 'none'
+    document.getElementById('conteudo').style.width = '100%'
+    document.getElementById('referencias').style.width = '100%'
+    document.getElementById('showMenu').style.display = 'inline'
+    document.getElementById('hideMenu').style.display = 'none'
+}
+
+function showMenu() {
+    document.getElementById('cabecalho').style.display = 'flex'
+    document.getElementById('conteudo').style.width = '80%'
+    document.getElementById('referencias').style.width = '80%';
+    document.getElementById('showMenu').style.display = 'none'
+    document.getElementById('hideMenu').style.display = 'inline'
+
 }
 
 function moreLayout(){
@@ -31,7 +50,7 @@ function incrementLetter() {
     }
 }
 
-function decrementLetter(params) {
+function decrementLetter() {
     fontsize--
     elements = document.getElementsByTagName('section')
     for (element of elements) {
