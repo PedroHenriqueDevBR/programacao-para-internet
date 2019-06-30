337675ce7b416fe4aa7f53e117b6c1bb1ca05594
pedrohenriquedevbr <p.lyma18@gmail.com>
2019-06-30 19:43:26 -0300

    Sistema de login criado

--- connectedin/perfis/models.py
+++ connectedin/perfis/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Perfil(models.Model):
     nome = models.CharField(max_length=255, null=False)
+    senha = models.CharField(max_length=100, null=False)
     telefone = models.CharField(max_length=15, null= False)
     email = models.CharField(max_length=255, null=False)
     nome_empresa = models.CharField(max_length=255, null=False)
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from perfis.models import Perfil, Convite
+from perfis import session, constants
+from django.contrib import messages
 
-
-# Create your views here.
+# # 
+# # Páginas
+# # 
 def index(request):
     perfil_logado = get_perfil_logado(request)
 
-    if len(perfil_logado) != 1:
+    if perfil_logado == None:
         return redirect('login')
 
     return render(request, 'index.html', {
@@ -16,16 +19,22 @@ def index(request):
 
 
 def login(request):
+
     if request.method == 'POST':
-        login = request.POST.get('login')
+        email = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        usuario_cadastrado = 
-
-        import pdb; pdb.set_trace()
-
+        if logar(request, email, senha):
+            return redirect('/')
+        else:
+            messages.add_message(request, messages.INFO, 'Usuário não cadastrado')
+        
     return render(request, 'login.html')
 
+
+# #
+# # Métodos auxiliares
+# #
 
 def exibir_perfil(request, perfil_id):
     dados = {}
@@ -33,24 +42,26 @@ def exibir_perfil(request, perfil_id):
     dados['perfil_logado'] = get_perfil_logado(request)
     # import pdb; pdb.set_trace()
     dados['ja_eh_contato'] = dados['perfil_logado'].contatos.all()
-
-
     return render(request, 'perfil.html', dados)
 
 
-def get(perfil_id):
-    return Perfil.objects.get(id=perfil_id)
+def logar(request, email, senha):
+    usuario = Perfil.objects.filter(email=email, senha=senha)
+    logado_com_sucesso = len(usuario) == 1
+    if logado_com_sucesso: session.adicionar_dado(request, constants.ID_USUARIO, str(usuario[0].id))
+    return logado_com_sucesso
 
 
 def get_perfil_logado(request):
-    return Perfil.objects.filter(id=1)
+    id_usuario = session.recuperar_dado(request, constants.ID_USUARIO)
+    if id_usuario == '': return None
+    return Perfil.objects.filter(id=id_usuario)
 
 
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
     perfil_logado.convidar(perfil_a_convidar)
-
     return redirect('index')
 
 
